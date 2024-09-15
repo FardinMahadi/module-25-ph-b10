4c70ba1685f8d2de14ec9b9bb80cd76c178e3c09
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-15 14:32:20 +0600

    update

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,19 @@
 "use strict";
 
-console.log("Hello World!");
-console.log(document);
+// console.log("Hello World!");
+// console.log(document);
+
+const sections = document.querySelectorAll("section");
+for (const section of sections) {
+  section.style.border = "2px solid steelblue";
+  section.style.marginBottom = "5px";
+  section.style.borderRadius = "15px";
+  section.style.paddingLeft = "7px";
+  section.style.backgroundColor = "lightblue";
+}
+
+// const placesContainer = document.getElementById("places-container");
+// placesContainer.style.backgroundColor = "yellow";
+
+const placesContainer = document.getElementById("places-container");
+placesContainer.classList.add("yellow-bg");
